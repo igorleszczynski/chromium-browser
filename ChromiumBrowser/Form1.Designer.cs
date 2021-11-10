65be4146c380d271b5aa4c15d0243f52a9a071e1
Igor Leszczyński <igorleszczynski99@gmail.com>
2021-11-10 09:01:25 +0100

    init

---
+++ ChromiumBrowser/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.f5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.devToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.f5ToolStripMenuItem,
+            this.devToolsToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // f5ToolStripMenuItem
+            // 
+            this.f5ToolStripMenuItem.Name = "f5ToolStripMenuItem";
+            resources.ApplyResources(this.f5ToolStripMenuItem, "f5ToolStripMenuItem");
+            this.f5ToolStripMenuItem.Click += new System.EventHandler(this.f5ToolStripMenuItem_Click);
+            // 
+            // devToolsToolStripMenuItem
+            // 
+            this.devToolsToolStripMenuItem.Name = "devToolsToolStripMenuItem";
+            resources.ApplyResources(this.devToolsToolStripMenuItem, "devToolsToolStripMenuItem");
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.globalWindowKeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.globalWindowKeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.globalWindowKeyUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem f5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem devToolsToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ChromiumBrowser/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.Example.Handlers;
+using CefSharp.WinForms;
+using ChromiumBrowser;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            var config = new IniFile("config.ini");
+            if (!config.KeyExists("homepage"))
+            {
+                config.Write("homepage", "http://www.google.com");
+            }
+            var homepage = config.Read("homepage");
+            chromeBrowser = new ChromiumWebBrowser(homepage);
+            chromeBrowser.DownloadHandler = new DownloadHandler();
+            this.panel1.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void f5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.chromeBrowser.Reload();
+        }
+
+        private void devToolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.chromeBrowser.ShowDevTools();
+        }
+
+        private void globalWindowKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("globalWindowKeyDown");
+        }
+
+        private void globalWindowKeyPress(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show("globalWindowKeyPress");
+        }
+
+        private void globalWindowKeyUp(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("globalWindowKeyUp");
+        }
+    }
+}
